5e80418b46744c3c9ff57631542db1061eddf470
Jon <jon@iglu.in.th>
2015-11-13 11:33:26 +0700

    new table, and click-compare functionality. Some styles too

--- js/script.js
+++ js/script.js
@@ -1,29 +1,92 @@
-//My comment (Jon)
-
 angular.module('myApp', [])
-.controller('myController', ['$scope',function myController($scope) {
-	$scope.season;
-	$scope.collection;
-	$scope.category;
-	$scope.styleColor;
-	$scope.styleNum;
-	$scope.fabric;
-	$scope.database = [];
-	$scope.submitButton = function() {
-		console.log($scope.season)
-		console.log($scope.collection)
-		console.log($scope.category)
-		console.log($scope.styleColor)
-		console.log($scope.styleNum)
-		console.log($scope.fabric)
-		var article = {
-			season: $scope.season,
-			collection: $scope.collection,
-			category: $scope.category,
-			color: $scope.styleColor,
-			styleNumber: $scope.styleNum,
-			fabric: $scope.fabric
-		}
-		$scope.database.push(article)
-	}
-}]);
+    .controller('myController', ['$scope', function myController($scope) {
+        $scope.season;
+        $scope.collection;
+        $scope.category;
+        $scope.styleColor;
+        $scope.styleNum;
+        $scope.fabric;
+        $scope.searchText;
+        $scope.database = [{
+            "season": "pf16",
+            "collection": "collection",
+            "category": "ow",
+            "color": "black",
+            "styleNumber": "1",
+            "fabric": "1234",
+            "$$hashKey": "object:3"
+        }, {
+            "season": "pf16",
+            "collection": "collection",
+            "category": "ow",
+            "color": "black",
+            "styleNumber": "2",
+            "fabric": "123",
+            "$$hashKey": "object:5"
+        }, {
+            "season": "pf16",
+            "collection": "collection",
+            "category": "ow",
+            "color": "black",
+            "styleNumber": "3",
+            "fabric": "123",
+            "$$hashKey": "object:7"
+        }, {
+            "season": "pf16",
+            "collection": "collection",
+            "category": "ow",
+            "color": "black",
+            "styleNumber": "4",
+            "fabric": "123",
+            "$$hashKey": "object:9"
+        }, {
+            "season": "pf16",
+            "collection": "collection",
+            "category": "ow",
+            "color": "black",
+            "styleNumber": "5",
+            "fabric": "123",
+            "$$hashKey": "object:11"
+        }];
+
+        $scope.compareArray = [];
+
+        $scope.submitButton = function () {
+            var article = {
+                season: $scope.season,
+                collection: $scope.collection,
+                category: $scope.category,
+                color: $scope.styleColor,
+                styleNumber: $scope.styleNum,
+                fabric: $scope.fabric
+            }
+            $scope.database.push(article);
+
+            $scope.clearForm();
+        }
+
+        $scope.clearForm = function() {
+            $scope.season = '';
+            $scope.collection = '';
+            $scope.category = '';
+            $scope.styleColor = '';
+            $scope.styleNum = '';
+            $scope.fabric = '';
+            $scope.searchText = '';
+        };
+
+        $scope.rowClicked = function(styleNumber) {
+            var position = $scope.compareArray.indexOf(styleNumber);
+
+            if(position == -1) {
+                $scope.compareArray.push(styleNumber);
+            } else {
+                $scope.compareArray.splice(position, 1);
+            }
+            console.log($scope.compareArray);
+        };
+
+        $scope.compare = function() {
+            console.log("I want to compare this articles: " + $scope.compareArray);
+        };
+    }]);
